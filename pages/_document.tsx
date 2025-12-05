4e11d1d97120c364566a960ea7668ea26eac84e8
Sarvesh <femfitwears@gmail.com>
2025-12-05 13:00:06 +0530

    Initial project upload

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,33 +1,64 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// pages/_document.tsx
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        {/* Google AdSense verification script */}
-        <script
-          async
-          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6803904743046507`}
-          crossOrigin="anonymous"
-        ></script>
-
-        {/* Optional fonts */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+/**
+ * Tell TypeScript about window.dataLayer so we don't get "cannot find name"
+ */
+declare global {
+  interface Window {
+    dataLayer: any[];
+  }
 }
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* Google AdSense verification script */}
+          <script
+            async
+            src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6803904743046507`}
+            crossOrigin="anonymous"
+          />
+
+          {/* Google Analytics (GA4) - gtag.js */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=G-R1B6KYTRTF`}
+          />
+          <script
+            // dangerouslySetInnerHTML must receive a string so JSX/TSX doesn't try to parse it
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-R1B6KYTRTF');
+              `,
+            }}
+          />
+
+          {/* Optional fonts */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+
+        <body className="antialiased">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
